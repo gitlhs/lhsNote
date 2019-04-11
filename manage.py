df7c7cee91e74c55f10d5f092af5f12c7f94437c
cloudisme <2462418994@qq.com>
2019-04-11 21:59:35 +0800

    删除无用文件

--- manage.py
+++
@@ -1,3 +0,0 @@
-import fcrm
-if __name__ == '__main__':
-    fcrm.app.run()
--- order.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-order = Blueprint("order",__name__)
-
-@order.route('/order')
-def register():   
-    return "order
