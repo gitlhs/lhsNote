02e65c59758c5fc049de659411a43f8f4d0abb67
lhs <1692339983@qq.com>
2019-04-15 23:24:16 +0800

    url

--- src/web/lnote/__init__.py
+++ src/web/lnote/__init__.py
@@ -8,6 +8,7 @@ lnote = Blueprint(
 	__name__,
 	template_folder = 'templates',   # 模板所在文件夹的名字
 	static_folder   = 'static',      # 静态文件所在文件的名字
+	static_url_path = '/s' 			# 静态文件访问url路径
 )
 
 from . import index
